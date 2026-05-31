57fa75c1bad095c7d83d2a37e5218514d6a12509
cvaldez97 <valdezcorbin1997@gmail.com>
2026-05-30 20:39:54 -0500

    added .gitignore, updated code

--- formula.py
+++ formula.py
@@ -1,31 +1,29 @@
-# Both formulas will take height in inches and convert it to cenimeters and weight in pounds and convert to kilograms
-# within the functions. 
+# Both formulas will take height in inches and convert it to cenimeters and weight in pounds and convert to kilograms within the functions. 
 
 # Male Formula 
 def BMR_M_equation(weight, height, age, activity):
-    return ((10 * (weight / 2.2)) + (6.25 * (height * 2.54)) - (5 * age) + 5) * activity
-# ---example formula to test if it works. Delete when ready to deploy-----
-weight = 220
-height = 73
-age = 29
-activity = 1
-
-BMR = int(BMR_M_equation(weight, height, age, activity))
-print(BMR)
+    if activity == "Little to no exercise.":
+        activity = 1.2
+    elif activity == "Light exercise 1-3 days/week.":
+        activity = 1.375
+    elif activity == "Moderate exercise 3-5 days/week.":
+        activity = 1.55
+    elif activity == "Hard exercise 6-7 days/week.":
+        activity = 1.725
+    else:
+        activity = 1.9
+    return int((10 * (weight / 2.2)) + (6.25 * (height * 2.54)) - (5 * age) + 5) * activity
 
 # Female equation 
 def BMR_F_equation(weight, height, age, activity):
+    if activity == "Little to no exercise.":
+        activity = 1.2
+    elif activity == "Light exercise 1-3 days/week.":
+        activity = 1.375
+    elif activity == "Moderate exercise 3-5 days/week.":
+        activity = 1.55
+    elif activity == "Hard exercise 6-7 days/week.":
+        activity = 1.725
+    else:
+        activity = 1.9
     return ((10 * (weight / 2.2)) + (6.25 * (height *2.54)) - (5 * age) - 161) * activity
-# ---example formula to test if it works. Delete when ready to deploy-----
-weight = 135
-height = 60
-age = 25
-activity = 1
-
-BMR = int(BMR_F_equation(weight, height, age, activity))
-print (BMR)
-
-# need to make a function that will convert BMR into macros
-# Function below will take your BMR and convert it to your macros via protein, fat and carbs. 
-
-    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import sys
+from formula import *
 
 # Typewriter effect
 def type_text(text, speed=0.05, new_line=True):
@@ -9,6 +10,7 @@ def type_text(text, speed=0.05, new_line=True):
         time.sleep(speed)
     if new_line:
         print()
+
 # Intro load in
 print("")
 type_text("Valdez Nutrition", 0.05)
@@ -25,6 +27,8 @@ def goal_confirm(name, path):
 def sex_confirm(name, sex):
     return f"You have selected '{sex}'. Is this correct?"
 
+def activity_confirm(name, activity):
+    return f"{name}, you have selected '{activity}'. Is this correct?"
 
 # --- Name Input ---
 type_text("Please enter your name: ", 0.05, new_line=False)
@@ -45,8 +49,8 @@ print("")
 type_text(f"{name}, lets get started with a few basic questions so that I can best assist you in your calorie and macro goals!", 0.05)
 type_text("-------------------", 0.1)
 print("")
-# --- Sex Input --- ### ask if sex input is correct  
 
+# --- Sex Input ---   
 sex_map = {
     "male": "Male",
     "female": "Female",
@@ -118,6 +122,8 @@ while True:
     except ValueError:
         type_text("Numbers only.\n")
 
+print("")
+
 while True:
     type_text("what's your height (inches)?: ", new_line=False)
     try: 
@@ -128,6 +134,8 @@ while True:
             type_text("Enter a valid height.\n")
     except ValueError:
         type_text("numbers only.\n")
+
+print("")
 
 while True:
     type_text("What is your current age?: ", new_line=False)
@@ -140,20 +148,55 @@ while True:
     except ValueError:
         type_text("Numbers only.\n")
 
+print("")
 
-# Disregard all code below this point for now
-# next step is to figure out activity level
-# from there use the formulas to calculate calories
-
-
-type_text(f"\nAwesome {name}, I have your current weight at {weight}, height at {height}, and your age at {age}. ")
-type_text("Is this correct? ")
-type_text("Type Yes or No:", new_line=False )
-confirm = input().strip().lower()
+activity_map = {
+    "1": "Little to no exercise.", 
+    "2": "Light exercise 1-3 days/week.",
+    "3": "Moderate exercise 3-5 days/week.",
+    "4": "Hard exercise 6-7 days/week.",
+    "5": "Very hard training, physical labor, or 2x/day traing."
+}
 
 while True:
-    if confirm in ["yes", "y"]:
-        break
-    else:
-        type_text("No problem, let's try that again.\n") #break infinite loop and make loop back over details 
+    type_text("Based off of the options below how would you gauge your level of activity?:", 0.05)
+    type_text("- 1: Little to no exercise."
+    "\n- 2: Light exercise 1-3 days/week." 
+    "\n- 3: Moderate exercise 3-5 days/week." 
+    "\n- 4: Hard exercise 6-7 days/week." 
+    "\n- 5: Very hard training, physical labor, or 2x/day training. ", 0.05)
+    type_text("please make a numeric decision by selecting 1 -5: ", 0.05, new_line=False)
+    activity_input = input().strip()
+    
+    if activity_input in activity_map:
+        activity = activity_map[activity_input]
+        print("")
+        type_text(activity_confirm(name, activity))
+        type_text("Type Yes or No: ", 0.05, new_line=False) 
+        confirm = input().strip().lower()
 
+        if confirm in ["yes", "y"]:
+            break
+        elif confirm in ["no", "n"]:
+            type_text("Alright, let's try again...\n")
+        else:
+            type_text("Please type Yes or No only.\n")
+    else:
+        type_text("Please select a valid option.\n")
+
+print("")
+
+type_text("One moment while I calculate your calories..")
+type_text("-------------------", 0.1)
+print("")
+
+if sex == "female":
+    BMR_F_equation(height, weight, age, activity)
+else:
+    MBMR = BMR_M_equation(weight, height, age, activity)
+
+type_text(f"{name}'s Daily Calories\n - {MBMR}")
+
+# Finish converting calories with activity for both male and female
+# Next adjust calories based on goal path
+# finally take final calories and convert them into macros
